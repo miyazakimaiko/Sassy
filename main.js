7035536bb9efd9a80465e7453a893081dd91ff40
miyazakimaiko <myzkmik19922@gmail.com>
2019-09-18 16:16:25 +0100

    Changed header animation

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-const header = document.querySelector(".header");
-const hero = document.querySelector(".header");
+window.onscroll = () => {
+    headerShrink();
+}
 
-window.addEventListener("scroll", () => {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        header.classList.toggle("header-transition");
+function headerShrink() {
+    const pos = document.documentElement.scrollTop;
+    const header = document.querySelector(".header");
+
+    if( pos > window.innerHeight ) {
+        header.style.backgroundColor = "rgb(7, 32, 70)";
+        header.style.height = "7vh";
+    } else {
+        header.style.backgroundColor = "transparent";
+        header.style.height = "10vh";
     }
-});
+}
 
 const menuTrigger = document.querySelector(".menu-trigger");
 const headerNav = document.querySelector(".header-nav");
@@ -14,3 +22,5 @@ const links = document.querySelectorAll(".nav-links li");
 menuTrigger.addEventListener("click", () => {
     headerNav.classList.toggle("open");
 });
+
+AOS.init();
